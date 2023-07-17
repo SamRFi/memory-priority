6ab0268f1b076a17d07649ca0b575b5504217bf2
sam.rafie <sam.rafie@student.howest.be>
2023-07-17 16:08:09 +0200

    can launch a login screen, fxml connected!

---
+++ src/main/java/memorypriority/ui/FxApplication.java
@@ -0,0 +1,24 @@
+package memorypriority.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class FxApplication extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/login.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
