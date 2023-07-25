3598ee4d01f857a2beb9f393868fb26a7b593c07
sam.rafie <sam.rafie@student.howest.be>
2023-07-26 01:11:43 +0200

    basics MemorySetService done

--- src/main/java/memorypriority/service/MemorySetService.java
+++ src/main/java/memorypriority/service/MemorySetService.java
@@ -2,11 +2,24 @@ package memorypriority.service;
 
 import memorypriority.data.MemorySetRepository;
 import memorypriority.data.Repositories;
+import memorypriority.domain.MemoryCollection;
+import memorypriority.domain.MemorySet;
+import memorypriority.domain.User;
 
 public class MemorySetService {
+    private final User loggedInUser;
     private final MemorySetRepository memorySetRepository;
 
-    public MemorySetService() {
+    public MemorySetService(User loggedInUser) {
         this.memorySetRepository = Repositories.getMemorySetRepository();
+        this.loggedInUser = loggedInUser;
+    }
+
+    public MemoryCollection getMemoryCollectionOfUser() {
+        return memorySetRepository.getMemoryCollectionOfUser(loggedInUser.getUsername());
+    }
+
+    public void addMemorySetToUser(MemorySet memorySet) {
+        memorySetRepository.addMemorySetToUser(loggedInUser.getUsername(), memorySet);
     }
 }
