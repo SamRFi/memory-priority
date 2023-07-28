d75c8aa8164c9436d3dcbe844a76d45b651aab5c
sam.rafie <sam.rafie@student.howest.be>
2023-07-28 17:56:16 +0200

    added dashboardcontroller, first version, still have to see if it works

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,17 +1,17 @@
 
 ]
 -src/main/java/memorypriority/util/Config.java,a\d\adb5186114032ff6ce602a0cb27333f7dfaed441
-f
-6src/main/java/memorypriority/domain/PriorityLevel.java,3\2\3214eb6d76d25dbd8ae49b2cbfdc239257b3c056
 ]
 -src/main/java/memorypriority/util/Crypto.java,4\9\49332121954a0fea3a5b267cd7b153058ffae428
 S
 #src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-]
--src/main/java/memorypriority/domain/User.java,e\f\ef05d33cd1337ca582806031f5584a1cdbdf6c27
 j
 :src/test/java/memorypriority/domain/AutoRehearsalTest.java,8\d\8d02db86d0edde1a01cfb84b4e5dae8f8a7a4fe4
 c
 3src/main/java/memorypriority/data/Repositories.java,9\5\95e471713896a1e86d20cdae4a252e6e2d9b95f0
+n
+>src/main/java/memorypriority/data/JdbcMemorySetRepository.java,e\e\eef0297a1bc47907d7b6026a4c2c9dad24ec36e2
+n
+>src/main/java/memorypriority/util/MemoryPriorityException.java,1\5\1556f7d799b54219901fe57ec2293ba7858fdd9b
--- src/main/java/memorypriority/ui/DashboardController.java
+++ src/main/java/memorypriority/ui/DashboardController.java
@@ -1,4 +1,81 @@
 package memorypriority.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import memorypriority.domain.MemoryCollection;
+import memorypriority.domain.MemorySet;
+import memorypriority.domain.User;
+import memorypriority.service.MemorySetService;
+
+import java.util.Set;
+
 public class DashboardController {
+
+    @FXML
+    private VBox highPriorityColumn;
+    @FXML
+    private VBox mediumPriorityColumn;
+    @FXML
+    private VBox lowPriorityColumn;
+
+    private MemorySetService memorySetService;
+
+    public DashboardController() {
+        this.memorySetService = new MemorySetService(new User("root", "root"));
+    }
+
+    @FXML
+    public void initialize() {
+        populateMemorySets();
+    }
+
+    // This should be called after the user logs in
+    public void populateMemorySets() {
+        MemoryCollection memoryCollection = memorySetService.getMemoryCollectionOfUser();
+        Set<MemorySet> memorySets = memoryCollection.getMemorySets();
+
+        for (MemorySet memorySet : memorySets) {
+            switch (memorySet.getPriorityLevel()) {
+                case HIGH:
+                    highPriorityColumn.getChildren().add(createMemorySetBox(memorySet));
+                    break;
+                case MEDIUM:
+                    mediumPriorityColumn.getChildren().add(createMemorySetBox(memorySet));
+                    break;
+                case LOW:
+                    lowPriorityColumn.getChildren().add(createMemorySetBox(memorySet));
+                    break;
+            }
+        }
+    }
+
+    private HBox createMemorySetBox(MemorySet memorySet) {
+        Label nameLabel = new Label(memorySet.getName());
+        Button rehearseButton = new Button("Rehearse");
+        rehearseButton.setOnAction(event -> rehearseMemorySet(memorySet));
+        Button increasePriorityButton = new Button("<");
+        increasePriorityButton.setOnAction(event -> increasePriority(memorySet));
+        Button decreasePriorityButton = new Button(">");
+        decreasePriorityButton.setOnAction(event -> decreasePriority(memorySet));
+
+        HBox memorySetBox = new HBox(nameLabel, rehearseButton, increasePriorityButton, decreasePriorityButton);
+        memorySetBox.setSpacing(10);
+
+        return memorySetBox;
+    }
+
+    private void rehearseMemorySet(MemorySet memorySet) {
+        // Implement rehearse logic
+    }
+
+    private void increasePriority(MemorySet memorySet) {
+        // Implement increase priority logic
+    }
+
+    private void decreasePriority(MemorySet memorySet) {
+        // Implement decrease priority logic
+    }
 }
