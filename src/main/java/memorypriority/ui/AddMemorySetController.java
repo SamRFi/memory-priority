19181dab0f7aa3c8ae3d80ed8a3fdd4f6713d243
sam.rafie <sam.rafie@student.howest.be>
2023-08-10 17:28:53 +0200

    added an empty controller

---
+++ src/main/java/memorypriority/ui/AddMemorySetController.java
@@ -0,0 +1,38 @@
+package memorypriority.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class AddMemorySetController {
+
+    @FXML
+    private GridPane keyValuePairsContainer;
+
+    @FXML
+    private void initialize() {
+        // This method will be called after all FXML components are loaded
+        // Here you can place initialization code if needed
+    }
+
+    @FXML
+    private void handleSearchButtonAction() {
+        // Implement the logic for the search button (🔍) here
+    }
+
+    @FXML
+    private void handleRemoveButtonAction() {
+        // Implement the logic for the remove button (❌) here
+    }
+
+    @FXML
+    private void handleAddMoreRowsButtonAction() {
+        // Implement the logic to add more key-value pairs rows
+    }
+
+    @FXML
+    private void handleSaveButtonAction() {
+        // Implement the logic to save key-value pairs
+    }
+}
